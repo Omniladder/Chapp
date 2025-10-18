14419a7dc27f153c12cf75d3496089c074e167d6
omniladder <dustintobrien@gmail.com>
2025-10-17 22:58:03 -0400

    Wrote some Getter functions for Achievements for big refactor

--- src/services/FriendFunctions/friendFunctions.ts
+++ src/services/FriendFunctions/friendFunctions.ts
@@ -1,148 +1,19 @@
-import express, { Request, response, Response } from 'express';
+import { Request } from 'express';
 import sequelize from '../../dbFiles/db';
 import { Op } from 'sequelize';
 
 import querySchema from './querySchema';
 import addFriendSchema from './addFriendSchema';
 import removeFriendSchema from './removeFriendSchema';
-
+import { calculateAchievements, getFriendsID } from './getAchievements'
 
 // DB ORM Object
 import { User } from "../../models/userSchema";
 import { Friend } from '../../models/friendSchema';
-import session from 'express-session';
-import { number, success } from 'zod';
 import { Conversation } from '../../models';
 
-type user = {
-    userID: number;
-    username: string;
-    fname: string;
-    lname: string;
-}
 
-
-async function calculateAchievements(userID: number): Promise<void>{
- 
-    // Sets all Friends to 0
-    console.log("Clearing Friend Achievements");
-    await Friend.update(
-        { isFoF: false, isRival: false, isBest: false, isMutualBest: false, isTop: false },
-        {
-            where: {
-                friendID1: userID
-            }
-        }
-    )
-
-   
-    //Top Friend Calculation ie. Top 5 Friends
-    console.log("Calculating Top 5 Friends");
-    let topFriends = await Friend.findAll({
-        where: {
-            friendID1: userID
-        },
-        attributes: ['friendID2'],
-        order: [['score', 'DESC']],
-        limit: 5
-    });
-
-    if(topFriends.length != 0){ //If You have friends lol.
-
-    await Friend.update(
-        {isTop: true},
-        {
-        where: {
-            friendID1: userID,
-            friendID2: {[Op.in]:  topFriends.map(r => r.friendID2)},
-            score: {[Op.gte]: 3}
-        }
-    })
-    
-    //Best Friend Calculation ie. You are my Highest Score Friend
-    //Mutual Best Friend Calculation ie. You are my best Friend I am yours
-    let bestFriendID = topFriends[0].friendID2;
-
-    if(bestFriendID){
-        console.log("Checking and Updating for Mutual Bestie");
-        let mutualBest = await Friend.findOne({
-            where: {
-                friendID1: bestFriendID,
-                friendID2: userID,
-                isBest: true
-            }
-        });
-
-        await Friend.update(
-            {isBest: true, isMutualBest: !!mutualBest},
-            {
-            where: {
-                friendID1: userID,
-                friendID2: bestFriendID
-            }
-        });
-    //Rival Calculation Your Best Friend is My Best Friend
-    let rivals = await Friend.findAll({
-        where: {
-            friendID2: bestFriendID,
-            isBest: true
-        },
-        attributes: ['friendID1']
-    })
-    if(rivals){
-    await Friend.update(
-        {isRival: true},
-        {
-            where: {
-                friendID1: userID,
-                friendID2: {[Op.in]: rivals.map( r => r.friendID1 )}
-            }
-        }
-    )
-    }
-    }
-
-    //Friend of Friend Calculation. We share a Friend
-    console.log("Setting Friend of Friends");
-    let friendIDs = await getFriendsID(userID);
-    let friendOfFriendsIDs = await Friend.findAll({
-        where: {
-            friendID1: {[Op.in]: friendIDs}
-        },
-        attributes: ['friendID2']
-    });
-
-    await Friend.update(
-        {isFoF: true},
-        {
-            where: {
-                friendID1: userID,
-                friendID2: {[Op.in]: friendOfFriendsIDs.map(r => r.friendID2)}
-            }
-        }
-    );
-    }
-}
-
-
-async function getFriendsID(userID: number): Promise<number[]>{
-   const friends = await Friend.findAll({
-        where:{
-            friendID1: userID 
-        },
-        attributes: ['friendID2'],
-        order: [['score', 'DESC']]
-   })
-
-    let friendIDs : number[] = [];
-    for (let friend of friends){
-        friendIDs.push(friend.friendID2);
-    }
-    return friendIDs
-}
-
-
-//TODO: Add Similarity Search
+// TODO: Move Queries to calculateAchievements
 export async function queryPeople(req: Request){
     
     const schemaTest = querySchema.safeParse(req.body);
---
+++ src/services/FriendFunctions/getAchievements.ts
@@ -0,0 +1,191 @@
+
+/**
+    * Author: Dustin O'Brien
+    * Description:
+    * File dedicated to helper functions for calculating and getting Achievements
+    *
+    * Functions:
+    * calculateAchievements(): Main function for calculating all Achievements
+*/
+
+import { Friend } from '../../models/friendSchema';
+import { Op, where } from 'sequelize';
+
+
+// Context Data Object used to Cache Results
+// Realistically I probably should've put this all in an object but I'm too lazy too now
+type AchievementContext = {
+    userID: number;
+    foundMutualBestie: boolean;
+    friendArray?: number[];
+    friendOfFriendArray?: number[];
+    bestFriend?: number;
+    rivalFriendArray?: number[];
+    top5Friends?: number[]
+    bestFriendBestie?: number;
+    mutualBestFriend?: number;
+}
+
+function makeContext(userID: number): AchievementContext {
+    let newContext: AchievementContext = {userID, foundMutualBestie: false}
+    return newContext;
+}
+
+/**
+* Calculates all Achievements relative to the given user
+* @param User to calculate Friend Achievements of
+*/
+export async function calculateAchievements(userID: number): Promise<void>{
+    await clearAchievements(userID);
+
+    let currentContext: AchievementContext = makeContext(userID);
+
+    await setFriendOfFriend(currentContext),
+    await setTop5(currentContext),
+    await setBestFriend(currentContext),
+    await setMutualBestFriend(currentContext),
+    await setRivalFriend(currentContext),
+}
+
+/**
+* Sets all Achievements friends basck to false
+*
+* @param userID - User to clear Achievements of
+*
+*/
+
+async function clearAchievements(userID: number): Promise<void>{
+    await Friend.update(
+        { isFoF: false, isRival: false, isBest: false, isMutualBest: false, isTop: false },
+        {
+            where: {
+                friendID1: userID
+            }
+        }
+    );
+}
+
+
+
+
+// GETTER METHODS FOR EACH CONTEXT
+
+/**
+* Returns list of friend ID's of a given user
+*
+* @param userID - The of user to fetch friends of
+* @param [limit] - Optional Parameter gets the top <limit> friends
+* @returns An array of Friends ID
+*
+*/
+export async function getFriendsID(currentContext: AchievementContext): Promise<number[]>{
+    if (currentContext.friendArray) return currentContext.friendArray; // return cache
+
+   currentContext.friendArray = (await Friend.findAll({
+        where:{
+            friendID1: currentContext.userID
+        },
+        attributes: ['friendID2'],
+        order: [['score', 'DESC']],
+   })).map(f => f.friendID2);
+
+    return currentContext.friendArray
+}
+
+/**
+* Returns list of friend ID's of a given user
+*
+* @param userID - The of user to fetch friends of
+* @returns An array of Friends ID
+*
+*/
+
+async function getFriendOfFriendID(currentContext: AchievementContext): Promise<number[]>{
+    if (currentContext.friendOfFriendArray) return currentContext.friendOfFriendArray
+
+    const FRIEND_IDS = await getFriendsID(currentContext);
+    currentContext.friendOfFriendArray = (await Friend.findAll({
+        where: {
+            friendID1: {[Op.in]: FRIEND_IDS}
+        },
+        attributes: ['friendID2']
+    })).map( f => f.friendID2 );
+
+    return currentContext.friendOfFriendArray;
+}
+
+/**
+    * Gets your number 1 most talked to friend
+* @param currentContext: The data variable for your firend
+* @returns number: bestFriend ID
+*/
+async function getBestFriendID(currentContext: AchievementContext): Promise<number>{
+    if(currentContext.bestFriend) return currentContext.bestFriend;
+    currentContext.bestFriend = (await getFriendsID(currentContext))[0];
+    return currentContext.bestFriend;
+}
+
+
+async function getMutualBestFriendID(currentContext: AchievementContext): Promise<number | undefined>{
+    if(currentContext.foundMutualBestie) return currentContext.mutualBestFriend;
+
+    currentContext.bestFriendBestie = (await getBestFriendBestieID(currentContext));
+
+    if (currentContext.bestFriendBestie == currentContext.userID){
+        currentContext.mutualBestFriend = await getBestFriendID(currentContext);
+    }
+    else {
+        currentContext.mutualBestFriend = undefined;
+    }
+
+    currentContext.foundMutualBestie = true;
+    return currentContext.mutualBestFriend;
+}
+
+async function getBestFriendBestieID(currentContext: AchievementContext): Promise<number>{
+    if(currentContext.bestFriendBestie) return currentContext.bestFriendBestie;
+
+    const BEST_FRIEND_ID: number = await getBestFriendID(currentContext);
+    let bestieContext: AchievementContext = makeContext(BEST_FRIEND_ID);
+
+    currentContext.bestFriendBestie = (await getFriendsID(bestieContext))[0];
+
+    return currentContext.bestFriendBestie;
+}
+
+async function getTop5FriendID(currentContext: AchievementContext): Promise<number[]> {
+    if(currentContext.top5Friends) return currentContext.top5Friends;
+
+    currentContext.top5Friends = (await getFriendsID(currentContext)).slice(0, 5);
+
+    return currentContext.top5Friends;
+}
+
+
+async function getRivalFriendsID(currentContext: AchievementContext): Promise<number[]> {
+    if (currentContext.rivalFriendArray) return currentContext.rivalFriendArray;
+
+    const BEST_FRIEND_ID = getBestFriendID(currentContext);
+
+    const RIVAL_FRIEND_IDS = (await Friend.findAll({
+        where: {
+            friendID2: BEST_FRIEND_ID,
+            isBest: true
+        },
+        attributes: ['friendID1']
+    })).map(f => f.friendID1);
+
+    return RIVAL_FRIEND_IDS
+}
+
+
+// SETTER METHODS FOR EACH ACHIEVEMENT
+
+
+
+
+
+
+
+
+
